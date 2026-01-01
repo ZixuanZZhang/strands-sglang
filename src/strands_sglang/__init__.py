dd14e44a2625ee14f2cc5bd4df108c5d222bbd0b
YuanHe <yuanhe.cs.ai@gmail.com>
2025-12-31 18:19:19 -0800

    chore: add license info

--- src/strands_sglang/__init__.py
+++ src/strands_sglang/__init__.py
@@ -1,12 +1,26 @@
+# Copyright 2025 Horizon RL Contributors
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from .sglang_model import SGLangModel
 from .token_manager import TokenManager
-from .tool_parser import HermesToolCallParser, ToolCall, ToolCallError, ToolCallParser
+from .tool_parser import PARSE_ERROR_TOOL_NAME, HermesToolCallParser, ToolCallParser, ToolCallParseResult
 
 __all__ = [
     "SGLangModel",
     "TokenManager",
-    "ToolCall",
-    "ToolCallError",
+    "ToolCallParseResult",
     "ToolCallParser",
     "HermesToolCallParser",
+    "PARSE_ERROR_TOOL_NAME",
 ]
